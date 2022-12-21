c415770a74a7a8ca22d4d72484d3194c334b66dd
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-12-21 11:56:18 +0100

    An empty board is now correctly generated in beginner mode

--- src/main/java/it/units/sdm/jminesweeper/Board.java
+++ src/main/java/it/units/sdm/jminesweeper/Board.java
@@ -1,4 +1,29 @@
 package it.units.sdm.jminesweeper;
 
+import java.awt.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Board {
+    private final Map<Point, String> gameBoard;
+    private final GameConfiguration gameConfiguration;
+
+    public Board(GameConfiguration gameConfiguration) {
+        gameBoard = new LinkedHashMap<>();
+        this.gameConfiguration = gameConfiguration;
+        fillBoard(gameConfiguration.dimension());
+    }
+
+    public Map<Point, String> getGameBoard() {
+        return gameBoard;
+    }
+
+    private void fillBoard(Dimension boardDimensions) {
+        for (int i = 0; i < boardDimensions.getHeight(); i++) {
+            for (int j = 0; j < boardDimensions.getWidth(); j++) {
+                gameBoard.put(new Point(i, j), "o");
+            }
+        }
+    }
+
 }
---
+++ src/main/java/it/units/sdm/jminesweeper/GameConfiguration.java
@@ -0,0 +1,7 @@
+package it.units.sdm.jminesweeper;
+
+import java.awt.*;
+
+public record GameConfiguration(Dimension dimension, int minesNumber) {
+
+}
